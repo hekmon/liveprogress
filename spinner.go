34bdbfce22687e52d842fc00af6d6f9beaf15a2b
Hekmon <3418467+hekmon@users.noreply.github.com>
2024-04-16 14:23:20 +0200

    Add a spinner

---
+++ spinner.go
@@ -0,0 +1,18 @@
+package liveprogress
+
+var (
+	spinnerStates = []rune{'⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏'}
+)
+
+// Spinner is a custom item that can be added as a custom line or part of a custom DecoratorFunc.
+type Spinner struct {
+	lastShow int
+}
+
+func (s *Spinner) String() string {
+	s.lastShow++
+	if s.lastShow >= len(spinnerStates) {
+		s.lastShow = 0
+	}
+	return string(spinnerStates[s.lastShow])
+}
