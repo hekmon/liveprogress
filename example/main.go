53adb7c83b4180e23cee55e36c2b83ae1198c466
Hekmon <3418467+hekmon@users.noreply.github.com>
2024-04-16 15:02:55 +0200

    add example

---
+++ example/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/hekmon/liveprogress"
+)
+
+var (
+	size1G = 1 * 1024 * 1024 * 1024
+	size3G = 3 * 1024 * 1024 * 1024
+	size7G = 7 * 1024 * 1024 * 1024
+)
+
+func main() {
+	// Config
+	liveprogress.SetProgressStyleUTF8Arrows()
+	var workers sync.WaitGroup
+	// Go
+	liveprogress.Start()
+	workers.Add(1)
+	go func() {
+		hashRandom(size3G)
+		workers.Done()
+	}()
+	workers.Add(1)
+	go func() {
+		hashRandom(size7G)
+		workers.Done()
+	}()
+	workers.Add(1)
+	go func() {
+		hashRandom(size1G)
+		workers.Done()
+	}()
+	// Wait
+	time.Sleep(10 * time.Millisecond)
+	liveprogress.AddCustom(&liveprogress.Spinner{})
+	workers.Wait()
+	liveprogress.Stop(true)
+}
+
+func hashRandom(size int) {
+	// Open random
+	fd, err := os.Open("/dev/random")
+	if err != nil {
+		panic(err)
+	}
+	defer fd.Close()
+	// Create the hasher
+	hasher := New(fd, size)
+	// Create the hasher progress bar
+	bar := liveprogress.AddBar(uint64(size))
+	bar.PrependFunc(func(bar *liveprogress.Bar) string {
+		return fmt.Sprintf("Hashing %d bytes ", size)
+	})
+	bar.AppendPercent()
+	bar.AppendFunc(func(bar *liveprogress.Bar) string {
+		return "  Remaining:"
+	})
+	bar.AppendTimeRemaining()
+	bar.AppendFunc(func(bar *liveprogress.Bar) string {
+		return fmt.Sprintf("  SHA256: %X", hasher.GetCurrentHash())
+	})
+	// Start hashing
+	err = hasher.ComputeHash(bar.CurrentAdd)
+	if err != nil {
+		panic(err)
+	}
+	// Hashing done
+	liveprogress.RemoveBar(bar)
+	fmt.Fprintf(liveprogress.Bypass(), "%d bytes file hash: %X\n", size, hasher.GetCurrentHash())
+}
